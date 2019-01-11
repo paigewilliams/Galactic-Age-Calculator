f1c493eaa1d1a35b118392e6f92e1317f6533789
Paige Williams <paw145@humboldt.edu>
2019-01-11 09:26:47 -0800

    Test for setting now as the current time; passing.

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -10,5 +10,11 @@ describe('Calculator', function(){
     expect(calc.lifeExpectancy).toEqual(79);
   });
 
+  it('takes the users birthday and life expectancy and sets now as the current time', function(){
+    let calc = new Calculator("1994-09-20", 79);
+    expect(calc.now.getUTCFullYear()).toEqual(2019);
+    expect(calc.now.getUTCDate()).toEqual(11);
+    expect(calc.now.getUTCMonth()).toEqual(0);
+  });
 
 });
