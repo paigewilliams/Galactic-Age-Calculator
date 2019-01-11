856c53ea0351f55fa74ffa52581e272a503d8444
Paige Williams <paw145@humboldt.edu>
2019-01-11 09:29:41 -0800

    Add before each in tests; passing.

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -2,8 +2,13 @@ import { Calculator } from './../src/calculator.js';
 
 describe('Calculator', function(){
 
+  let calc;
+
+  beforeEach(function(){
+    calc = new Calculator("1994-09-20", 79);
+  });
+
   it('takes a the users birthday and life expectancy', function(){
-    let calc = new Calculator("1994-09-20", 79);
     expect(calc.birthday.getUTCDate()).toEqual(20);
     expect(calc.birthday.getUTCMonth()).toEqual(8);
     expect(calc.birthday.getUTCFullYear()).toEqual(1994);
@@ -11,7 +16,6 @@ describe('Calculator', function(){
   });
 
   it('takes the users birthday and life expectancy and sets now as the current time', function(){
-    let calc = new Calculator("1994-09-20", 79);
     expect(calc.now.getUTCFullYear()).toEqual(2019);
     expect(calc.now.getUTCDate()).toEqual(11);
     expect(calc.now.getUTCMonth()).toEqual(0);
